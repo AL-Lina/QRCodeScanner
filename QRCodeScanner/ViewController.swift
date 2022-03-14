2b2355ec17553301da520c83591a5eda01f9a860
AL-Lina <lina-sokolova00@mail.ru>
2022-03-14 13:20:51 +0300

    Initial Commit

---
+++ QRCodeScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeScanner
+//
+//  Created by Alina Sokolova on 14.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
